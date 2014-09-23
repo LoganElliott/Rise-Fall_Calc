30e03dc32913990687ce177fbc6210c7bd9fc380
Logan Elliott <logansails@gmail.com>
2014-09-23 22:02:18 +1200

    Adds source files

---
+++ SourceFiles/src/com/example/imagecircle/DoublePoint.java
@@ -0,0 +1,31 @@
+package com.example.imagecircle;
+
+/**
+ * Author: Logan Elliott
+ * Program: Image Circle Calculator and diagram
+ * Date: 6/11/2013
+ * Version: 1.5
+ * 
+ * Additonal Info:
+ *  4" = 101.6
+ *  5" = 127mm
+ **/
+
+public class DoublePoint {
+	private double x;
+	private double y;
+	
+	public DoublePoint(double x, double y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double getX(){
+		return x;
+	}
+	
+	public double getY(){
+		return y;
+	}
+
+}
